7e2a27f76f6a5746c8350aa8241e0a9d8eb91030
EMichira <emmanuelmichira@gmail.com>
2017-08-27 14:44:15 +0300

    Added tests and logic to assign and access IDs

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,5 +5,21 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Client {
-  
+  private String description;
+  private int id;
+
+// constructor client
+public Client (String description) {
+  this.description = description;
+}
+
+//to get description
+public String getDescription() {
+  return description;
+}
+
+// to get ids of the clients
+    public int getId() {
+      return id;
+    }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -13,5 +13,28 @@ public class ClientTest {
       assertEquals(true, Client.all().get(0).equals(firstClient));
       assertEquals(true, Client.all().get(1).equals(secondClient));
     }
+    //  test to check whether can recognize similar objects in true manner
+      @Test
+      public void equals_returnsTrueIfDescriptionsAretheSame() {
+        Client firstClient = new Client("Jessicah", 1);
+        Client secondClient = new Client("Jessicah", 1);
+        assertTrue(firstClient.equals(secondClient));
+      }
+      //assign and access IDs
+      @Test
+      public void getId_clientInstantiateWithAnID() {
+        Client myClient = new Client("Jessicah", 1);
+        myClient.save();
+        assertTrue(myClient.getId() > 0);
+      }
+      //locating specific Tasks using their unique ID
+      @Test
+      public void find_returnsClientWithSameId_secondClient() {
+        Client firstClient = new Client("Jessicah", 1);
+        firstClient.save();
+        Client secondClient = new Client("Edna", 1);
+        secondClient.save();
+        assertEquals(Client.find(secondClient.getId()), secondClient);
+      }
 
 }
