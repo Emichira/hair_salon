71ba670a8c1b94de9b3cf40c7b4b08a023623ddb
EMichira <emmanuelmichira@gmail.com>
2017-08-27 14:13:22 +0300

    Added tests to confirm if we can successfully instantiate Client objects

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -3,5 +3,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ClientTest {
+  //  test to return all client instances as true
+    @Test
+    public void all_returnsAllInstancesOfClient_true() {
+      Client firstClient = new Client("Jessicah", 1);
+      firstClient.save();
+      Client secondClient = new Client("Edna", 1);
+      secondClient.save();
+      assertEquals(true, Client.all().get(0).equals(firstClient));
+      assertEquals(true, Client.all().get(1).equals(secondClient));
+    }
 
 }
