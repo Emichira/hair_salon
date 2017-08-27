49746f14d03deb453ac5ab0c24eb1ab990757a8d
EMichira <emmanuelmichira@gmail.com>
2017-08-27 23:04:30 +0300

    Added restful routes to render necessary templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,13 +9,38 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-// root route for rendering our home page
-  get("/", (request, response) -> {
-   Map<String, Object> model = new HashMap<String, Object>();
-   model.put("stylists", Stylist.all());
-   model.put("template", "templates/index.vtl");
-   return new ModelAndView(model, layout);
-   }, new VelocityTemplateEngine());
+    // root route for rendering our home page
+      get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("stylists", Stylist.all());
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+   // route responsible for rendering the template with new-client form
+      get("/clients/new", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/client-form.vtl");
+        return new ModelAndView(model, layout);
+       }, new VelocityTemplateEngine());
+
+   // route to display all clients
+     get("/clients", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("clients", Client.all());
+        model.put("template", "templates/clients.vtl");
+        return new ModelAndView(model, layout);
+       }, new VelocityTemplateEngine());
+
+   // route for gathering information and displaying success template
+      post("/clients", (request,response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        String description = request.queryParams("description");
+        Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+        Client newClient = new Client(description, stylist.getId());
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
   }
 }
