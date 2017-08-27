ada17fca789a88d05f5ca831a65272f0ff121d74
EMichira <emmanuelmichira@gmail.com>
2017-08-27 20:56:49 +0300

    Added test and logic to file

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -4,5 +4,14 @@ import java.util.ArrayList;
 import org.sql2o.*;
 
 public class Stylist {
-  
+  private String description;
+
+  public Stylist(String description) {
+    this.description = description;
+  }
+
+  //returns the description of the stylist
+    public String getDescription() {
+     return description;
+   }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -2,5 +2,12 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class StylistTest {
+  //  test to check whether similar objects are returned true
+  @Test
+      public void equals_returnsTrueIfDescriptionsAretheSame() {
+        Stylist firstStylist = new Stylist("Janet");
+        Stylist secondStylist = new Stylist("Janet");
+        assertTrue(firstStylist.equals(secondStylist));
+      }
 
 }
