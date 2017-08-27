534aa430c08ef62f7dc0511fd00508a6fd0f82a9
EMichira <emmanuelmichira@gmail.com>
2017-08-27 21:04:48 +0300

    Added tests to check if class can instantiate successfully

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -2,12 +2,22 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class StylistTest {
-  //  test to check whether similar objects are returned true
+  //  test to return all stylist instances as true
   @Test
       public void equals_returnsTrueIfDescriptionsAretheSame() {
         Stylist firstStylist = new Stylist("Janet");
         Stylist secondStylist = new Stylist("Janet");
         assertTrue(firstStylist.equals(secondStylist));
       }
+  // test to return all instances of stylists as true
+  @Test
+     public void all_returnsAllInstancesOfStylist_true() {
+       Stylist firstStylist = new Stylist("Janet");
+       firstStylist.save();
+       Stylist secondStylist = new Stylist("Lilian");
+       secondStylist.save();
+       assertEquals(true, Stylist.all().get(0).equals(firstStylist));
+       assertEquals(true, Stylist.all().get(1).equals(secondStylist));
+     }
 
 }
