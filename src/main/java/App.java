d6067b2f8c7f3d91914625e6a2552a797216bfa6
EMichira <emmanuelmichira@gmail.com>
2017-08-28 19:40:35 +0300

    Added restful routes in the App file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,12 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+        port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+        port = 4567;
+    }
 
     // root route for rendering our home page
       get("/", (request, response) -> {
@@ -85,15 +91,51 @@ public class App {
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
 
-      // indicating stylist has already  done the work
-      post("/stylists", (request, response) -> {
-        Map<String, Object> model = new HashMap<String, Object>();
-        String description = request.queryParams("description");
-        Stylist newStylist = new Stylist(description);
-        newStylist.save();
-        model.put("template", "templates/stylist-success.vtl");
-        return new ModelAndView(model, layout);
-      }, new VelocityTemplateEngine());
+      // linking client with stylist
+        get("/stylists/:stylist_id/clients/:id", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
+          Client client = Client.find(Integer.parseInt(request.params(":id")));
+          model.put("stylist", stylist);
+          model.put("client", client);
+          model.put("template", "templates/client.vtl");
+          return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
+
+      // a route to process new-client form submission
+        post("/clients", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+          String description = request.queryParams("description");
+          Client newClient = new Client(description, stylist.getId());
+          newClient.save();
+          model.put("stylist", stylist);
+          model.put("template", "templates/stylist-client-success.vtl");
+          return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
+
+      // enhancing particular clients belong to a particular stylist
+        post("/stylists/:stylist_id/clients/:id", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          Client client = Client.find(Integer.parseInt(request.params("id")));
+          String description = request.queryParams("description");
+          Stylist stylist = Stylist.find(client.getStylistId());
+          client.update(description);
+          String url = String.format("/stylists/%d/clients/%d", stylist.getId(), client.getId());
+          response.redirect(url);
+          return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
+
+      // deleting clients who are already serviced
+        post("/stylists/:stylist_id/clients/:id/delete", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          Client client = Client.find(Integer.parseInt(request.params("id")));
+          Stylist stylist = Stylist.find(client.getStylistId());
+          client.delete();
+          model.put("stylist", stylist);
+          model.put("template", "templates/stylist.vtl");
+          return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
 
   }
 }
